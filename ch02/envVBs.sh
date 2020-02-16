539856aad5f9ed9184f41bb02492e2d8f62c0694
jiamin.luu <jiamin.luu@hhu.edu.cn>
2020-02-16 16:14:11 +0800

    initialize

---
+++ ch02/envVBs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#set VINEB and VINLB in /etc/bash.bashrc and ~/.bashrc, respectively
+echo $VINEB 
+echo $VINLB
+
+#set $BASH_ENV as hello.rc, in order to execute the init file during the non-interactive mode
+echo $HELLO
---
+++ ch04/compile/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "function.h"
+
+using namespace std;
+
+void print(int a, int b);
+
+int main(){
+ 
+  int a , b; 
+  int c;
+
+  cout << "input a: ";
+  cin >> a;
+  cout << "input b: ";
+  cin >> b;
+
+
+  cout << "At first, ";
+  print(a, b);
+
+  swap(&a, &b);
+  cout << "After swaping, ";
+  print(a, b);
+
+
+  a= increaseBy(a, 5);
+  cout << "After adding a by 5, ";
+  print(a, b);
+
+}
+
+void print( int a, int b ){
+  cout << "a is " << a << ", and b is " << b << endl;
+}

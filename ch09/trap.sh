99685a3bff103635b9d38a227df6063756850207
jiamin@Groot <jiamin.luu@hhu.edu.cn>
2021-02-24 09:52:17 +0800

    add a new demo about signal trapping

---
+++ ch09/trap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Testing signal trapping
+
+trap "echo 'Trap signal SIGINT'" SIGINT
+trap "echo 'Trap when the script exit'" EXIT
+count=1
+while [ $count -le 10 ]; do
+  echo "Loop #$count"
+  sleep 1
+  let count++
+done
+
+echo "The script ends"
+
